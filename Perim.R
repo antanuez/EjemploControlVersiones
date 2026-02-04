fe05d5a9cb7f61622b04fe334778d3a15078aff7
antanuez <antanuez@alumni.uv.es>
2026-02-04 19:23:23 +0100

    Último commit con los errores solucionados

--- Perim.R
+++ Perim.R
@@ -6,11 +6,10 @@ LongitudCircunf<-function(r){
   return(2*3.14159*r)
 }
 
-<<<<<<< HEAD
 AreaTrapecio<-function(b,B,h){
-  return(((B+b)*h)/2)
-=======
+  return(((B+b)*h)/2) 
+}
+
 PerimetroRectangulo<-function(L,l){
   return(2*(L+l) )
->>>>>>> Rama2
 }
